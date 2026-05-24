f1aa45326799329301f74978c0b315152fabdaae
Anton Gimpel <water34545@gmail.com>
2026-05-24 11:46:44 -0300

    Initial smart lamp firmware

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#define FASTLED_RMT_BUILTIN_DRIVER 1
+#include <WiFi.h>
+#include <FastLED.h>
+
+#define LED_PIN     2
+#define NUM_LEDS    120
+#define BRIGHTNESS  80
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
+
+// WiFi
+const char* ssid = "VirreyTelecentro";
+const char* password = "virrey2428644";
+
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // LED setup
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS); // Яркость (0-255)
+
+  // WiFi
+  connectWiFi();
+}
+
+void loop() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Yellow;   // Задаем красный цвет
+    FastLED.show();        // Отправляем команду на отображение
+    FastLED.delay(50);
+    leds[i] = CRGB::Black; // Выключаем светодиод перед следующим шагом
+  }
+}
