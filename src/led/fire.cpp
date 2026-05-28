b2fdeeb51edff4f83d52773c7c413528436b7eb3
Anton Gimpel <water34545@gmail.com>
2026-05-27 22:48:23 -0300

    gyver fire

--- src/led/fire.cpp
+++ src/led/fire.cpp
@@ -3,104 +3,129 @@
 
 #include <FastLED.h>
 
-static uint8_t ff_y = 0;
-static uint8_t ff_z = 0;
+#define FIRE_WIDTH MATRIX_WIDTH
+#define FIRE_HEIGHT MATRIX_HEIGHT
 
-static float sparkX[4];
-static float sparkY[4];
+static uint8_t matrixValue[FIRE_HEIGHT][FIRE_WIDTH];
+static uint8_t line[FIRE_WIDTH];
 
-static bool initialized = false;
+static uint8_t pcnt = 0;
 
-CRGBPalette16 firePalette =
-    HeatColors_p;
+const uint8_t valueMask[FIRE_HEIGHT][FIRE_WIDTH] = {
+    {32, 0, 0, 0, 0, 0, 0, 32},
+    {64, 0, 0, 0, 0, 0, 0, 64},
+    {96, 32, 0, 0, 0, 0, 32, 96},
+    {128, 64, 32, 0, 0, 32, 64, 128},
+    {160, 96, 64, 32, 32, 64, 96, 160},
+    {192, 128, 96, 64, 64, 96, 128, 192},
+    {255, 160, 128, 96, 96, 128, 160, 255},
+    {255, 192, 160, 128, 128, 160, 192, 255},
+    {255, 220, 185, 150, 150, 185, 220, 255},
+    {255, 255, 220, 185, 185, 220, 255, 255},
+    {255, 255, 255, 220, 220, 255, 255, 255},
+    {255, 255, 255, 255, 255, 255, 255, 255},
+    {255, 255, 255, 255, 255, 255, 255, 255},
+    {255, 255, 255, 255, 255, 255, 255, 255},
+    {255, 255, 255, 255, 255, 255, 255, 255}
+};
 
-void initFire() {
+const uint8_t hueMask[FIRE_HEIGHT][FIRE_WIDTH] = {
+    {1, 8, 16, 24, 24, 16, 8, 1},
+    {1, 8, 16, 20, 20, 16, 8, 1},
+    {1, 8, 13, 19, 19, 13, 8, 1},
+    {1, 5, 11, 13, 13, 11, 5, 1},
+    {1, 5, 11, 11, 11, 11, 5, 1},
+    {0, 1, 5, 8, 8, 5, 1, 0},
+    {0, 0, 1, 5, 5, 1, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+};
 
-    for (uint8_t i = 0; i < 4; i++) {
+void generateLine() {
 
-        sparkY[i] = random8(MATRIX_HEIGHT);
-        sparkX[i] = random8(MATRIX_WIDTH);
+    for (uint8_t x = 0; x < FIRE_WIDTH; x++) {
+
+        line[x] = random8(120, 255);
+    }
+}
+
+void shiftUp() {
+
+    for (int y = FIRE_HEIGHT - 1; y > 0; y--) {
+
+        for (int x = 0; x < FIRE_WIDTH; x++) {
+
+            matrixValue[y][x] = matrixValue[y - 1][x];
+        }
     }
 
-    initialized = true;
+    for (int x = 0; x < FIRE_WIDTH; x++) {
+
+        matrixValue[0][x] = line[x];
+    }
+}
+
+void drawFrame(uint8_t pcnt) {
+
+    for (int y = FIRE_HEIGHT - 1; y > 0; y--) {
+
+        for (int x = 0; x < FIRE_WIDTH; x++) {
+
+            int value =
+                (
+                    (100 - pcnt) * matrixValue[y][x]
+                    +
+                    pcnt * matrixValue[y - 1][x]
+                ) / 100;
+
+            value -= valueMask[y][x];
+
+            value = max(0, value);
+
+            leds[XY(x, y)] = CHSV(
+                8 + hueMask[y][x],
+                255,
+                value
+            );
+        }
+    }
+
+    for (int x = 0; x < FIRE_WIDTH; x++) {
+
+        leds[XY(x, 0)] = CHSV(
+            8 + hueMask[0][x],
+            255,
+            (
+                (100 - pcnt) * matrixValue[0][x]
+                +
+                pcnt * line[x]
+            ) / 100
+        );
+    }
 }
 
 void renderFire() {
 
-    if (!initialized) {
-        initFire();
+    if (pcnt >= 100) {
+
+        shiftUp();
+
+        generateLine();
+
+        pcnt = 0;
     }
 
-    uint8_t scaleX = 40;
-    uint8_t scaleY = 80;
-
-    for (uint8_t x = 0; x < MATRIX_WIDTH; x++) {
-
-        for (uint8_t y = 0; y < MATRIX_HEIGHT; y++) {
-
-            uint8_t noise = inoise8(
-                x * scaleX,
-                (y * scaleY) + ff_y,
-                ff_z
-            );
-
-            uint8_t fade =
-                map(y, 0, MATRIX_HEIGHT - 1, 255, 32);
-
-            uint8_t index =
-                qsub8(noise, 255 - fade);
-
-            CRGB color =
-                ColorFromPalette(
-                    firePalette,
-                    index,
-                    255,
-                    LINEARBLEND
-                );
-
-            leds[XY(x, MATRIX_HEIGHT - 1 - y)] = color;
-        }
-    }
-
-    // Sparks
-    for (uint8_t i = 0; i < 4; i++) {
-
-        uint8_t sx = sparkX[i];
-        uint8_t sy = sparkY[i];
-
-        if (sy < MATRIX_HEIGHT) {
-
-            leds[XY(sx, sy)] += CRGB(255, 180, 80);
-
-            leds[XY(sx, sy)].fadeToBlackBy(sy * 12);
-        }
-
-        sparkY[i] += 0.35;
-
-        if (random8() < 40) {
-
-            int move = random8(3) - 1;
-
-            sparkX[i] =
-                (uint8_t)((int)sparkX[i] + move + MATRIX_WIDTH)
-                % MATRIX_WIDTH;
-        }
-
-        if (sparkY[i] >= MATRIX_HEIGHT) {
-
-            sparkY[i] = random8(3);
-
-            sparkX[i] = random8(MATRIX_WIDTH);
-        }
-    }
-
-    ff_y += 3;
-
-    if (ff_y & 0x01) {
-        ff_z++;
-    }
+    drawFrame(pcnt);
 
     FastLED.show();
+
+    pcnt += 25;
 
     delay(30);
 }
